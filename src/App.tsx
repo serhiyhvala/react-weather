5dd3c39d0517bdcad5f6b495404a76c5fafe297d
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-07 09:39:09 +0300

    ADD: React-Router To Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home/components/Home'
+import { MonthStatistics } from './pages/MonthStatistics/components/MonthStatistics'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/month-statistics" element={<MonthStatistics />} />
+      </Routes>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
 import './styles/index.scss'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
-);
-
+)
