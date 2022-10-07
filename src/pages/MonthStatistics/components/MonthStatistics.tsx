2547f11e5d754a793ba55cb98c2ae9ad2e2fa754
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-07 09:26:13 +0300

    CREATE: Month Statistics Component

---
+++ src/pages/MonthStatistics/components/MonthStatistics.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './MonthStatistics.module.scss'
+type Props = {}
+
+export const MonthStatistics = (props: Props) => {
+  return (
+    <div className={styles.home}>Month Statistics</div>
+  )
+}
