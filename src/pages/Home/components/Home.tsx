66b73fa71606d3327484e0a3cd0db1378d7213d1
serhiy hvala <serhiyhvaladew@gmail.com>
2022-10-07 09:25:54 +0300

    CREATE: Home Component

---
+++ src/pages/Home/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './Home.module.scss'
+type Props = {}
+
+export const Home = (props: Props) => {
+  return (
+    <div className={styles.home}>Home</div>
+  )
+}
